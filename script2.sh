d4b7604e4446d45c827c5a25735b4463fb6039de
ales231 <alalbanto@uide.edu.ec>
2025-12-03 15:51:45 +0000

    arsenal

--- script2.sh
+++ script2.sh
@@ -1,4 +1,19 @@
 uname -a
+#Muestra toda la información del sistema operativo, incluyendo:
+#Tipo de kernel (Linux)
+#Nombre del equipo
+#Versión del kernel
+#Arquitectura (x86_64, arm64, etc.)
+#Básicamente: información general del sistema operativo.
 lscpu
+#Muestra información detallada del procesador (CPU):
+#Número de núcleos
+#Número de hilos
+#Arquitectura
+#Velocidad del CPU
+#Modelo
+#Cache
+#Sirve para saber exactamente qué CPU tiene tu máquina.
 free -h
+
 df -h
