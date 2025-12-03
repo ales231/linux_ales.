79575ce9adc5b42578ca96c30edd4958649f8c58
ales231 <alalbanto@uide.edu.ec>
2025-12-03 15:53:19 +0000

    arsenal

--- script2.sh
+++ script2.sh
@@ -15,5 +15,14 @@ lscpu
 #Cache
 #Sirve para saber exactamente qué CPU tiene tu máquina.
 free -h
-
+#Muestra la memoria RAM disponible y usada.
+#El parámetro -h significa human readable, es decir, lo muestra en MB, GB, etc.
+#Puedes ver cuánta RAM tienes, cuánta está libre y cuánta está en uso.
 df -h
+#Muestra el espacio disponible en los discos del sistema.
+#Muestra las particiones
+#El total de almacenamiento
+#Cuánto está ocupado
+#Cuánto está libre
+#-h también significa human readable → GB, TB, etc.
+#Sirve para saber cuánto espacio tienes en el disco.
