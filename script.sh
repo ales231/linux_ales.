9da451fbe5546949aafa8f1a22d94b48d5d08fe5
ales231 <alalbanto@uide.edu.ec>
2025-12-03 15:41:41 +0000

    arsenal

---
+++ script.sh
@@ -0,0 +1,11 @@
+#Crear y listar archivos
+echo "Hola linux" > archivo.txt  
+#echo imprime un textoecho imprime un texto.
+#> redirige ese texto hacia un archivo.
+#Si el archivo no existe, lo crea.
+#Si existe, lo sobrescribe.
+cat archivo.txt  #cat muestra en pantalla lo que contiene el archivo llamado archivo.txt
+cp archivo.txt copia.txt   #
+ls -l
+rm archivo.txt
+
